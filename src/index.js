b5b7beb5920e296d6159a08c4f272f1a27fdde05
Friedger Müffke <mail@friedger.de>
2022-02-02 15:20:55 +0100

    replace panda with mirlo

--- src/index.js
+++ src/index.js
@@ -10,19 +10,13 @@ import { StacksMainnet } from "@stacks/network";
 import React, { useState } from "react";
 
 import ReactDOM from "react-dom";
-import { PandaMintContract, PandaNftContract } from "./artifacts/contracts.ts";
+import { MirloContract } from "./artifacts/contracts.ts";
 
 const userSession = new UserSession();
 const network = new StacksMainnet();
 
-const functions = [
-  PandaMintContract.Functions.Mint,
-  PandaMintContract.Functions.MintTwo,
-  PandaMintContract.Functions.MintThree,
-  PandaMintContract.Functions.MintFour,
-  PandaMintContract.Functions.MintFive,
-];
-const baseUri = "ipfs://QmXxAG3j9JDsXNWMeKsca6djYsi9hhtWCZT9JjoA81SsZu/{id}.json";
+const functions = ["claim", "claim-three", "claim-five", "claim-ten"];
+const numbers = [1, 3, 5, 10];
 
 export function App() {
   const [user, setUser] = useState();
@@ -31,9 +25,6 @@ export function App() {
 
   if (user || userSession.isUserSignedIn()) {
     const profile = userSession.loadUserData()?.profile;
-    const isContractOwner =
-      profile?.stxAddress?.mainnet ===
-      "SP1T4Y4WK9DGZ2EDWSNHRE5HRRBPVG7S46JAHW552";
     return (
       <div className="mint-container">
         <button
@@ -44,11 +35,11 @@ export function App() {
         >
           -
         </button>
-        <div className="count">{count}</div>
+        <div className="count">{numbers[count]}</div>
         <button
           onClick={() => {
             setStatus(undefined);
-            setCount(count + 1 > 5 ? 5 : count + 1);
+            setCount(count + 1 > 4 ? 4 : count + 1);
           }}
         >
           +
@@ -59,8 +50,8 @@ export function App() {
             setStatus(undefined);
             const fn = functions[count - 1];
             openContractCall({
-              contractAddress: PandaMintContract.address,
-              contractName: PandaMintContract.name,
+              contractAddress: "SP1QSA6J2HC2TV5NV0Q7X1H6ZBV6SDTDKC4GXBJPY",
+              contractName: "mirlos-for-artists-2022",
               functionName: fn.name,
               functionArgs: [],
               anchorMode: AnchorMode.ANY,
@@ -69,7 +60,7 @@ export function App() {
                 makeStandardSTXPostCondition(
                   profile.stxAddress.mainnet,
                   FungibleConditionCode.Equal,
-                  count * 25_000_000
+                  numbers[count] * 40_000_000
                 ),
               ],
               userSession,
@@ -100,103 +91,6 @@ export function App() {
           Disconnect
         </button>
         {status && <div className="status">{status}</div>}
-
-        {isContractOwner && (
-          <div>
-            <br />
-            <button
-              onClick={() => {
-                setStatus(undefined);
-                openContractCall({
-                  contractAddress: PandaMintContract.address,
-                  contractName: PandaMintContract.name,
-                  functionName:
-                    PandaMintContract.Functions.FlipMintpassSale.name,
-                  functionArgs: [],
-                  anchorMode: AnchorMode.ANY,
-                  userSession,
-                  network,
-                  onFinish: () => {
-                    setStatus(
-                      PandaNftContract.Functions.FlipMintpassSale.name +
-                        " transaction sent"
-                    );
-                  },
-                });
-              }}
-            >
-              Start Premint Sale
-            </button>
-
-            <br />
-            <button
-              onClick={() => {
-                setStatus(undefined);
-                openContractCall({
-                  contractAddress: PandaMintContract.address,
-                  contractName: PandaMintContract.name,
-                  functionName: PandaMintContract.Functions.FlipSale.name,
-                  functionArgs: [],
-                  anchorMode: AnchorMode.ANY,
-                  userSession,
-                  network,
-                  onFinish: () => {
-                    setStatus(
-                      PandaNftContract.Functions.FlipSale.name + " transaction sent"
-                    );
-                  },
-                });
-              }}
-            >
-              Start Public Sale
-            </button>
-            <br />
-            <button
-              onClick={() => {
-                setStatus(undefined);
-                openContractCall({
-                  contractAddress: PandaNftContract.address,
-                  contractName: PandaNftContract.name,
-                  functionName: PandaNftContract.Functions.SetBaseUri.name,
-                  functionArgs: [stringAsciiCV(baseUri)],
-                  anchorMode: AnchorMode.ANY,
-                  userSession,
-                  network,
-                  onFinish: () => {
-                    setStatus(
-                      PandaNftContract.Functions.SetBaseUri.name + " transaction sent"
-                    );
-                  },
-                });
-              }}
-            >
-              Reveal Pandas ({baseUri})
-            </button>
-            <br />
-            <button
-              onClick={() => {
-                setStatus(undefined);
-                openContractCall({
-                  contractAddress: PandaNftContract.address,
-                  contractName: PandaNftContract.name,
-                  functionName: PandaNftContract.Functions.FreezeMetadata.name,
-                  functionArgs: [],
-                  anchorMode: AnchorMode.ANY,
-                  userSession,
-                  network,
-                  onFinish: () => {
-                    setStatus(
-                      PandaNftContract.Functions.FreezeMetadata.name +
-                        " transaction sent"
-                    );
-                  },
-                });
-              }}
-            >
-              Freeze Metadata (can be called only once)
-            </button>
-          </div>
-        )}
       </div>
     );
   } else {
@@ -206,7 +100,7 @@ export function App() {
           onClick={() =>
             showConnect({
               appDetails: {
-                name: "Stacks Panda",
+                name: "Mirlo Mint",
                 icon: "https://youthful-wing-084af9.netlify.app/stackschina.png",
               },
               onFinish: () => {
@@ -215,7 +109,7 @@ export function App() {
             })
           }
         >
-          Mint a Stacks Panda
+          Mint a Mirlo NFT
         </button>
       </div>
     );
